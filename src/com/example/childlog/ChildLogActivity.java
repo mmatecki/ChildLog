e13d3de384f31eb7882234cb668410aab861184c
Łukasz Siwiński <lsiwinski@gmail.com>
2012-02-18 10:15:51 +0100

    ChildLog project created

---
+++ src/com/example/childlog/ChildLogActivity.java
@@ -0,0 +1,13 @@
+package com.example.childlog;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChildLogActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
