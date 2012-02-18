a0a29643fcb2e0c2d85a89ba7bf65fb2e29ed2b7
Łukasz Siwiński <lsiwinski@gmail.com>
2012-02-18 11:53:15 +0100

    Aktywnosci dodawacz zdarzen i lista zdarzen

---
+++ src/com/example/childlog/EventAdder.java
@@ -0,0 +1,12 @@
+package com.example.childlog;
+
+import android.app.Activity;
+
+/** Aktywnosc bedzie obslugiwac piewszy ekran aplikacji **/
+public class EventAdder extends Activity{
+
+	/** draft **/
+	public void onClickButton(){
+		// do nothing
+	}
+}
