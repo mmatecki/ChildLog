ccfd2ac0fc4287c79625e343586b3fb49c64546c
Maria Skrzypek <mmskrzyp@gmail.com>
2012-02-18 16:10:04 +0100

    zabawa z bazą danych

--- src/com/example/childlog/events/FoodActivity.java
+++ src/com/example/childlog/events/FoodActivity.java
@@ -1,15 +1,24 @@
 package com.example.childlog.events;
 
 import com.example.childlog.R;
+import com.example.childlog.db.EventsDataSource;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
 public class FoodActivity extends Activity {
+	private Context context = this;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.karmienie);
+		
+		EventsDataSource db = new EventsDataSource(context);
+		db.open();
+		db.createEvent("fooooooooood");
+		db.close();
 	}
 
 }
